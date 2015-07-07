a505f295598055f734173402a4269a2c233ecc03
caoruidong <crdfrank@gmail.com>
2015-07-07 18:30:48 +0800

    update for settextvalue

--- ldtpeditorts.py
+++ ldtpeditorts.py
@@ -1,13 +1,13 @@
 #coding=utf-8
 import os,sys,shutil
 
-##path=sys.argv[1]
-##dest=sys.argv[2]
-path='D:\\gedit.py'
-dest='D:\\share\\gedit.py'
+path=sys.argv[1]
+dest=sys.argv[2]
+##path='D:\\gedit.py'
+##dest='D:\\share\\gedit.py'
 
 reserve={'wait':'wait(%s)','click':'click("%s-%s.png")','waittillguiexist':'wait("%s.png",FOREVER)',
-         'waittillguinotexist':'waitVanish("%s.png",FOREVER)'}
+         'waittillguinotexist':'waitVanish("%s.png",FOREVER)','settextvalue':'type(%s)'}
 
 file=open(path)
 ftw=open(dest,'w') 
@@ -19,7 +19,9 @@ for line in file:
             if item[0]=='wait':
                 ftw.write(line.lstrip())
             elif item[0]=='click':
-                ftw.write(reserve[item[0]]%(item[1].strip('(",*'),item[2].strip('")*'))+'\n')
+                ftw.write(reserve[item[0]]%(item[1].strip('(",)*'),item[2].strip('"()*'))+'\n')
+            elif item[0]=='settextvalue':
+                ftw.write(reserve[item[0]]%(item[3].strip('(,*)'))+'\n')
             else:
                 ftw.write(reserve[item[0]]%(item[1].strip('(",)*'))+'\n')               
 file.close()
