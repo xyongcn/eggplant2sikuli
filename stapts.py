25ce132c2b78b6ff42fdd4e6a1d51561e9f42648
caoruidong <crdfrank@gmail.com>
2015-08-21 17:06:54 +0800

    calc full test

--- stapts.py
+++ stapts.py
@@ -3,17 +3,26 @@ import os,sys,shutil
 
 ##path=sys.argv[1]
 ##dest=sys.argv[2]
-path='D:\\calculator_result.result'
+path='D:\\calculator-test-full'
 dest='D:\\share\\calculator.py'
 
-file=open(path)
-ftw=open(dest,'w') 
-ftw.write('import shutil\n') 
+file=open(path,encoding='utf-8')
+ftw=open(dest,'w',encoding='utf-8') 
+##ftw.write('import shutil\n') 
 for line in file:
     if line!='\n':
         item=line.split(' : ')
-        if item[3] == 'math_equation_insert_digit':
-            ftw.write('click("frmCalculator-btnNumeric%s.png")\n'%item[-1][-2])        else:
+        if item[3] == 'gtk_button_clicked':
+            if item[-1][0]=='×':
+                subst='*'
+            elif item[-1][0]=='−':
+                subst='-'
+            elif item[-1][0]=='√':
+                subst='root'
+            else:
+                subst=item[-1][0]
+            ftw.write('click("frmCalculator-btnNumeric%s.png")\n'%subst)
+        else:
             ftw.write('click("%s.png")\n'%item[3])           
 file.close()
 ftw.close()
